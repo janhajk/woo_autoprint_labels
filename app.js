8598323793b59736c805d9add0dfa4323b08330a
Jan <janhajk@gmail.com>
2018-11-05 14:31:56 +0000

    add

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const config = require(__dirname + '/config.js');
+const request = require('request');
 var WooCommerceAPI = require('woocommerce-api');
  
 
@@ -20,8 +21,23 @@ var printed = [];
      for(var i in d) {
         console.log(d[i].id);
         console.log(d[i].shipping);
+        print_ql();
      }
    });
+
+
+var print_ql = function(text) {
+   var host = config.ql_api;
+   var full_cmd = host + "text?font_size=50&font_family=Minion%20Pro%20(%20Semibold%20)&text=" + text;
+   request(full_cmd, {
+   }, (err, res, body) = > {
+      if(err) {
+         return console.log(err);
+      }
+      console.log(body.url);
+      console.log(body.explanation);
+   });
+};
 
 // Cron
 //setInterval(function() {
--- sample.config.js
+++ sample.config.js
@@ -1,3 +1,4 @@
 exports.url = "";
 exports.consumerKey = "";
 exports.consumerSecret = "";
+exports.ql_api = "http://192.168.1.24:8013/api/print/";
